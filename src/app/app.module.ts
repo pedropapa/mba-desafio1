53823321506d87968d24b36daaf5159378d3a10d
Pedro Papadópolis <pplp93@gmail.com>
2017-07-27 11:42:21 -0300

    feat(app): Implementação inicial do projeto.
    @TODO Corrigir visualização das imagens no ios.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,38 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Camera } from "@ionic-native/camera";
+import {IonicStorageModule} from "@ionic/storage";
+import {ImagePicker} from "@ionic-native/image-picker";
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Camera,
+    ImagePicker
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,80 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, NgZone} from "@angular/core";
+import {ToastController} from "ionic-angular";
+import {Camera, CameraOptions} from "@ionic-native/camera";
+import {Storage} from "@ionic/storage";
+import {ImagePicker, ImagePickerOptions} from "@ionic-native/image-picker";
+import {DomSanitizer, SafeUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public imagesGallery: Array<SafeUrl> = [];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public toastCtrl: ToastController, public camera: Camera, public storage: Storage, public imagePicker: ImagePicker, public ngzone: NgZone) {
+    storage.get('gallery').then((gallery) => {
+      console.log('>>>>>> ', gallery);
+      if(gallery) {
+        this.imagesGallery = gallery;
+      }
+    });
   }
 
+  addFromCamera = () => {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      targetWidth: 90,
+      targetHeight: 90,
+      saveToPhotoAlbum: false
+    };
+
+    this.ngzone.run(() => {
+      this.camera.getPicture(options).then((imageData) => {
+        console.log(imageData);
+        this.addImageToGallery(imageData);
+      }, (err) => {
+        if (err == 'cordova_not_available') {
+          return this.toastMessage("Disponível apenas em dispositivos reais");
+        }
+
+        this.toastMessage(err);
+      });
+    })
+  };
+
+  addFromGallery = () => {
+    const options: ImagePickerOptions = {
+      quality: 100,
+      outputType: this.camera.DestinationType.DATA_URL
+    };
+
+    this.imagePicker.getPictures(options).then((results) => {
+      for(let x in results) {
+        this.addImageToGallery(results[x]);
+      }
+    }, (err) => {
+      if (err == 'cordova_not_available') {
+        return this.toastMessage("Disponível apenas em dispositivos reais");
+      }
+
+      this.toastMessage(err);
+    });
+  };
+
+  toastMessage = (message: string) => {
+    this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    }).present();
+  }
+
+  addImageToGallery = (img: string) => {
+      this.imagesGallery.push('data:image/jpeg;base64,' + img);
+      this.storage.set('gallery', this.imagesGallery);
+  };
 }
